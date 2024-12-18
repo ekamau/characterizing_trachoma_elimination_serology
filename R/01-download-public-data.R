b6ab8f7a229c0f3719a6465125d4144875e53f2f
everlyn kamau <everlyn.kamau@gmail.com>
2024-12-18 13:28:44 -0800

    add data downloading script

---
+++ R/01-download-public-data.R
@@ -0,0 +1,27 @@
+#----------------------------------
+# Characterizing trachoma elimination using serology
+#
+# Download .rds versions of public datasets from osf.io 
+# and save them in the local working repository:
+# ~/trachoma-serology/data
+#
+# data are available here: https://osf.io/jmtpz/
+#----------------------------------
+
+
+#----------------------------------
+# preamble
+#----------------------------------
+library(here)
+
+# source configuration file
+source(here("R","0-config.R"))
+
+# https://osf.io/9p3jn
+df_v4 <- osf_retrieve_file("9p3jn") %>%
+  osf_download(path=here("data"), conflicts = "overwrite", progress = TRUE)
+
+#----------------------------------
+# Session info
+#----------------------------------
+sessionInfo()
